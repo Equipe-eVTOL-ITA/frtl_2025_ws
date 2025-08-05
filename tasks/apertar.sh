952b7f10960aafebef2ccc93035bb02767e85553
evtolITA <evtol.ita@gmail.com>
2025-08-05 15:28:05 -0300

    mavlink_router and apertar scripts (Jetson)

---
+++ tasks/apertar.sh
@@ -0,0 +1,2 @@
+ #!/bin/bash
+ python3 /home/evtol/jetson-gpio/samples/servoControl.py
---
+++ tasks/mavlink_router.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "abrindo mavlink-router"
+sudo mavlink-routerd -c /etc/mavlink-router/main.conf
